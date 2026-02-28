7987c11625d9a80337df8e5555d2f19732536074
Senrian <47714364+Senrian@users.noreply.github.com>
2026-02-28 14:12:04 +0800

    Add main application class

---
+++ src/main/java/com/miniagent/MiniAgentApplication.java
@@ -0,0 +1,12 @@
+package com.miniagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniAgentApplication.class, args);
+    }
+}
